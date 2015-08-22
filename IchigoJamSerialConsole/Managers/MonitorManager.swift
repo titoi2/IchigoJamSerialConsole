dc5e5fb6b3a295e5fe7d77d7db012a3c87a29e80
titoi2 <aboutsoft.jp@gmail.com>
2015-08-22 15:47:20 +0900

    スクロール処理実装

--- IchigoJamSerialConsole/Managers/MonitorManager.swift
+++ IchigoJamSerialConsole/Managers/MonitorManager.swift
@@ -27,7 +27,7 @@ class MonitorManager:NSObject {
     var cursorY:Int = 0
     
     var vram:[[UInt8]]
-//    var screenImage: NSImage
+    //    var screenImage: NSImage
     
     enum InterpreterState {
         case Idle
@@ -52,24 +52,70 @@ class MonitorManager:NSObject {
                 putChar( x, y: y, c: 0x20)
             }
         }
-
+        
     }
     
+    /*
+    CLS 19, 12
+    SCROLL 左　21, 28
+    SCROLL 右　21, 29
+    SCROLL 上　21, 30
+    SCROLL 下　21, 31
+    LOCATE 21, 32+X,32+Y
+    */
     
     func interpret(str:[UInt8]) {
         for c in str {
             switch interpreterState {
             case .TakeX:
-                cursorX = Int(c) - 32 - 1
-                interpreterState = .TakeY
+                switch c {
+                case 28:
+                    // スクロール左
+                    scrollLeft()
+                    interpreterState = .Idle
+                    vram2ImageCG()
+                    break
+                case 29:
+                    // スクロール右
+                    scrollRight()
+                    interpreterState = .Idle
+                    vram2ImageCG()
+                    break
+                case 30:
+                    // スクロール上
+                    scrollUp()
+                    interpreterState = .Idle
+                    vram2ImageCG()
+                    break
+                case 31:
+                    // スクロール下
+                    scrollDown()
+                    interpreterState = .Idle
+                    vram2ImageCG()
+                    break
+                    
+                default:
+                    cursorX = Int(c) - 32
+                    
+                    if cursorX < 0 {
+                        NSLog("invalid cursorX:\(cursorX)")
+                        cursorX = 0
+                        interpreterState = .Idle
+                    } else {
+                        interpreterState = .TakeY
+                    }
+                    break
+                }
+                
+                
                 break
                 
             case .TakeY:
-                cursorY = Int(c) - 32 - 1
+                cursorY = Int(c) - 32
                 interpreterState = .Idle
                 break
                 
-
+                
                 
             default:
                 switch c {
@@ -118,26 +164,31 @@ class MonitorManager:NSObject {
     
     
     private func putChar(x:Int,y:Int,c:UInt8) {
-        
+        if x < 0 {
+            NSLog("invalid x:\(x)")
+        }
+        if y < 0 {
+            NSLog("invalid y:\(y)")
+        }
         vram[y][x] = c
         
     }
     
-
+    
     // lockFocusによるイメージ生成
     func vram2ImageLF() {
         
         var screenImage = NSImage(size: NSSize(width: self.width * 8,height: self.height * 8))
-
+        
         screenImage.lockFocus()
         
         for y in 0..<height {
             let yp = (height - y) * 8
-//            NSLog("y:\(y) yp:\(yp)")
+            //            NSLog("y:\(y) yp:\(yp)")
             for x in 0..<width {
                 
                 let c:UInt8 = vram[y][x]
-
+                
                 let xp = x * 8
                 let rect = NSRect(x: xp, y: yp, width: 8, height: 8)
                 fontImage.drawInRect(rect,
@@ -175,10 +226,10 @@ class MonitorManager:NSObject {
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.PremultipliedLast.rawValue)
         let bitmapContext = CGBitmapContextCreate(nil, widthBits, heightBits, bitsPerComponent, bytesPerRow, colorSpace, bitmapInfo)!
-
-//        CGContextSetRGBFillColor(bitmapContext, 1.0, 0.0, 0.0, 1.0);
-//        CGContextSetRGBStrokeColor(bitmapContext, 0.0, 1.0, 0.0, 1.0);
-//        CGContextFillRect(bitmapContext, NSRect(x: 0,y: 0,width: widthBits,height: heightBits));
+        
+        //        CGContextSetRGBFillColor(bitmapContext, 1.0, 0.0, 0.0, 1.0);
+        //        CGContextSetRGBStrokeColor(bitmapContext, 0.0, 1.0, 0.0, 1.0);
+        //        CGContextFillRect(bitmapContext, NSRect(x: 0,y: 0,width: widthBits,height: heightBits));
         
         for y in 0..<height {
             let yp =  (height - y - 1)  * 8
@@ -188,22 +239,22 @@ class MonitorManager:NSObject {
                 
                 let xp = x * 8
                 let rect = NSRect(x: xp, y: yp, width: 8, height: 8)
-
+                
                 let fontref = CGImageCreateWithImageInRect(image, fontRect(c));
-
+                
                 CGContextDrawImage(bitmapContext, rect, CGImageCreateCopy( fontref))
             }
         }
-
+        
         // ビットマップを NSImage に変換します。
         let newImageRef = CGBitmapContextCreateImage(bitmapContext)!
         let newImage = NSImage(CGImage: newImageRef, size: NSSize(width: widthBits, height: heightBits))
-
+        
         if let d = delegate {
             d.onDispChange(newImage)
         }
     }
-
+    
     func fontRect(c8:UInt8) -> NSRect {
         let c = Int(c8)
         let low = c & 0xF
@@ -211,6 +262,46 @@ class MonitorManager:NSObject {
         return NSRect(x: low * 8, y: high * 8, width: 8, height: 8)
     }
     
+    func scrollLeft() {
+        for y in 0..<height {
+            for x in 0..<(width - 1) {
+                vram[y][x] = vram[y][x+1]
+            }
+            vram[y][width - 1] = 32
+        }
+    }
+
+    func scrollRight() {
+        for y in 0..<height {
+            for var x = (width - 1); x > 0; x-- {
+                vram[y][x] = vram[y][x-1]
+            }
+            vram[y][0] = 32
+        }
+    }
+    
+    func scrollUp() {
+        for y in 0..<(height - 1) {
+            for x in 0..<width  {
+                vram[y][x] = vram[y+1][x]
+            }
+        }
+        for x in 0..<width  {
+            vram[height - 1][x] = 32
+        }
+    }
+
+    func scrollDown() {
+        for var y = (height - 1); y > 0; y-- {
+            for x in 0..<width  {
+                vram[y][x] = vram[y-1][x]
+            }
+        }
+        for x in 0..<width  {
+            vram[0][x] = 32
+        }
+    }
+
 
     func takeImage() {
         vram2ImageCG()
